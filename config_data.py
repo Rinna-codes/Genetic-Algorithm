acc515d368340e84212d72d3d06e6f6689f83fda
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-11-28 11:40:50 -0600

    started the different activities

--- config_data.py
+++ config_data.py
@@ -8,3 +8,9 @@ STARTING_MUT_RATE = 0.01
 THRESHOLD = 20 # Generations that don't have improvement before mutation 
 
 OUTPUT_FILE = "schedule_output.txt"
+
+"""Definitions following Appendix A"""
+ACTIVITIES = {
+    "SLA101A" : {"enrollment": 40, "prefer" : ["Glen", "Lock", "Banks"], "others" : ["Numen", "Richards", "Shaw", "Singer"]}, 
+    "SLA101B" : {"enrollment" : 35, "prefer" : ["Glen", "Lock", "Banks"], "others" : ["Numen", "Richards", "Shaw", "Singer"]}
+}

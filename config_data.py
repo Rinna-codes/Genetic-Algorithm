5ae7a1583f5fad31b805674f1ab1e0a15df6bccd
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-11-28 11:27:04 -0600

    main configurations + constraints

---
+++ config_data.py
@@ -0,0 +1,10 @@
+"""Configuration + Scheduling Data File"""
+
+"""Main Configurations + Constraints"""
+
+POP_SIZE = 500 
+MAX_GEN = 500
+STARTING_MUT_RATE = 0.01
+THRESHOLD = 20 # Generations that don't have improvement before mutation 
+
+OUTPUT_FILE = "schedule_output.txt"

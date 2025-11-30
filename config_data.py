0c13580b76656f782b31fc3e2825348a589363ac
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-11-30 12:14:27 -0600

    defined the activities

--- config_data.py
+++ config_data.py
@@ -13,5 +13,13 @@ OUTPUT_FILE = "schedule_output.txt"
 ACTIVITIES = {
     "SLA101A" : {"enrollment": 40, "prefer" : ["Glen", "Lock", "Banks"], "others" : ["Numen", "Richards", "Shaw", "Singer"]}, 
     "SLA101B" : {"enrollment" : 35, "prefer" : ["Glen", "Lock", "Banks"], "others" : ["Numen", "Richards", "Shaw", "Singer"]},
-    "SLA191A" : {"enrollment": }
+    "SLA191A" : {"enrollment": 45, "prefer" : ["Glen", "Lock", "Banks"], "others" : ["Numen", "Richards", "Shaw", "Singer"]},
+    "SLA191B" : {"enrollment" : 40, "prefer" : ["Glen", "Lock", "Banks"], "others" : ["Numen", "Richards", "Shaw", "Singer"]},
+    "SLA201" : {"enrollment" : 60, "prefer" : ["Glen", "Banks", "Zeldin", "Lock", "Singer"], "others" : ["Richards", "Uther", "Shaw"]},
+    "SLA291" : {"enrollment" : 50, "prefer" : ["Glen", "Banks", "Zeldin", "Lock", "Singer"], "otheres" : ["Richards", "Uther", "Shaw"]},
+    "SLA303" : {"enrollment": 25, "prefer" : ["Glen", "Zeldin"], "others" : ["Banks"]},
+    "SLA304" : {"enrollment" : 20, "prefer" : ["Singer", "Uther"], "others" : ["Richards"]},
+    "SLA394" : {"enrollment" : 15, "prefer" : ["Tyler", "Singer"], "others" : ["Richards", "Zeldin"]},
+    "SLA449" : {"enrollment" : 30, "prefer" : ["Tyler", "Zeldin", "Uther"], "others" : ["Zeldin", "Shaw"]},
+    "SLA451" : {"enrollment" : 90, "prefer" : ["Lock", "Banks", "Zeldin"], "others" : ["Tyler", "Singer", "Shaw", "Glen"]}
 }

ab1c3337a804672ef3c0634946c0e911ec0923f4
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-11-30 16:33:53 -0600

    finish the schedule class

--- schedule.py
+++ schedule.py
@@ -10,4 +10,20 @@ class Schedule:
     def initialization(self):
         """Create the initiliation of the population"""
         for activity in ACTIVITIES:
-            r = random
+            r = random.choice(ROOM_NAMES)
+            t = random.choice(TIME)
+            f = random.choice(FACILTATORS)
+            self.genes[activity] = (r, t, f)  # assign into the randomly generated tuple 
+    
+    def __str__(self):
+        lines = []
+        # format the output information
+        lines.append(f"{"Activity" : < 10} | {"Room" : < 12} | {"Time" : < 6} | {"Faciltator"}") # put into lines to display
+        lines.append("*" * 45)
+
+        sort_genes = sorted(self.genes.items(), key=lambda x: (TIME_MAP[x[1][1]], x[1][0])) # creates a list of tuples that is sorted by time slot and room name 
+
+        # format the output information
+        for acts, (r, t, f) in sort_genes:
+            lines.append(f"{acts:<10} | {r:<12} | {t:<6} | {f}")
+        return "\n".join(lines)
